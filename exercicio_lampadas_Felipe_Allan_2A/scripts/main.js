154af6f1d1f0594c69b3f623f317a6e396e2a157
EA - Felipe Allan Nascimento Cruz <felipe_allan@educadventista.org>
2024-04-24 17:07:58 -0300

    exercicios finalizados

---
+++ exercicio_lampadas_Felipe_Allan_2A/scripts/main.js
@@ -0,0 +1,32 @@
+const myImage1 = document.querySelector('#myImage1')
+const myImage2 = document.querySelector('#myImage2')
+
+const btn1 = document.querySelector('#btn1')
+const btn2 = document.querySelector('#btn2')
+
+myImage1.onclick = () => {
+    console.log(myImage1.src.indexOf("imagens/pic_bulbon.gif") != -1)
+    if(myImage1.src.indexOf("imagens/pic_bulbon.gif") != -1){
+        myImage1.src = 'imagens/pic_bulboff.gif'    
+    } else if(myImage1.src.indexOf("imagens/pic_bulboff.gif") != -1){
+        myImage1.src = 'imagens/pic_bulbon.gif'
+    }
+}
+myImage2.onclick = () => {
+    console.log(myImage2.src.indexOf("imagens/pic_bulbon.gif") != -1)
+    if(myImage2.src.indexOf("imagens/pic_bulbon.gif") != -1){
+        myImage2.src = 'imagens/pic_bulboff.gif'    
+    } else if(myImage2.src.indexOf("imagens/pic_bulboff.gif") != -1){
+        myImage2.src = 'imagens/pic_bulbon.gif'
+    }
+}
+
+ btn1.onclick = () => {
+     myImage1.src = './imagens/pic_bulbon.gif',
+    myImage2.src = './imagens/pic_bulboff.gif'
+    }
+
+tn2.onclick = () => {
+    myImage2.src = './imagens/pic_bulbon.gif',
+    myImage1.src = './imagens/pic_bulboff.gif'
+}
